3ff8a1cd3cfd004cba9261e02b351c43fda6c34e
Asif Shaikh <ripclaw_ffb@hotmail.com>
2021-02-05 20:03:29 -0800

    Upgrade to v17-10.0.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -15,7 +15,7 @@ if($packageParameters.keys.count -gt 0) {
   $silentArgs   = '/S'
 }
 
-$url        = 'https://cdn.unrealengine.com/CrossToolchain_Linux/v16_clang-9.0.1-centos7.exe'
+$url        = 'https://cdn.unrealengine.com/CrossToolchain_Linux/v17_clang-10.0.1-centos7.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -24,7 +24,7 @@ $packageArgs = @{
 
   softwareName  = 'Unreal Linux Toolchain*'
 
-  checksum      = 'faffaabbbc5de462284335ed4ea461e1305ae955a7cba285da0b38645c421d1b'
+  checksum      = '31b7641815f54f9e1c058e3318425e75833bf709f19a0c35a9b6ae8be8a9f4cd'
   checksumType  = 'sha256'
 
   silentArgs = $silentArgs
